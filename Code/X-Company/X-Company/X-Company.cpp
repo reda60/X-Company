d5af6117d8afd75455139dd3f0c2f0a75ca10800
fmutin <fmutin@IF501-210-13>
2018-05-15 08:40:25 +0200

    creation projet

---
+++ Code/X-Company/X-Company/X-Company.cpp
@@ -0,0 +1,14 @@
+// X-Company.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Bonjour" << endl;
+    return 0;
+}
+
